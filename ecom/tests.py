9d0294ecb59cfa34c3b28717110867c9ad086035
subina20 <khadkasubina20@gmail.com>
2022-02-21 20:54:09 +0545

    tset

--- ecom/tests.py
+++ ecom/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+# Create your tests here.
+from ecom.views import contactus_view
+
+from django.urls import reverse,resolve
+
+
+
+from django.test import Client, SimpleTestCase, TestCase
 
 # Create your tests here.
+
+
+
+class Testurls(SimpleTestCase):
+    
+    def test_resolve_to_Homepage(self):
+
+        url = reverse("ecommerce: contactus")
+
+        resolver = resolve(url)
+
+        self.assertEquals(resolver,contactus_view)
+
+ 
+
+
+
+
+
+
+
+
+
+
